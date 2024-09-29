bc3a68838623e7e6d12d7ce4b2d9320f1a922c69
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-09-29 08:34:10 +0100

    Book Keeping Model created

--- src/main/java/com/semicolon/africa/data/models/BookKeeping.java
+++ src/main/java/com/semicolon/africa/data/models/BookKeeping.java
@@ -1,15 +1,27 @@
 package com.semicolon.africa.data.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.semicolon.africa.data.type.PaymentMethod;
+import com.semicolon.africa.data.type.TransactionType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
 public class BookKeeping {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    private String receiverName;
+
+    private String senderName;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
 }
